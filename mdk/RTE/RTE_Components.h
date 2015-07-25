9fadc4ea5143579816daf17aa0ae1a0f556abbef
Manuel Limones <limonesu.me@gmail.com>
2015-07-25 18:19:39 -0500

    Rename keil project

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'SPL_10_ADC_Polling_STM32F0' 
+ * Project: 'SPL_12_ADC_DMA_STM32F0' 
  * Target:  'Target 1' 
  */
 
